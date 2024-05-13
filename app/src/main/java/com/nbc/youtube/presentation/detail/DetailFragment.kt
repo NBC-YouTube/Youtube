5bda9e6f632a2e54f4cd213cb8661c8e2a9c6863
DoDoBest <send1684@gmail.com>
2024-05-13 14:31:09 +0900

    feat: 기본 Fragment 생성
    Co-authored-by: Yejin-Byun <opkile@naver.com>
    Co-authored-by: jinnnyyy <jyk0435@naver.com>
    Co-authored-by: Sth-bear <pencil2486@gmail.com>
    Co-authored-by: jeong995 <ibaggim049@gmail.com>

---
+++ app/src/main/java/com/nbc/youtube/presentation/detail/DetailFragment.kt
@@ -0,0 +1,33 @@
+package com.nbc.youtube.presentation.detail
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.nbc.youtube.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private var _binding: FragmentDetailBinding? = null
+    private val binding: FragmentDetailBinding
+        get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentDetailBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+
+        super.onDestroyView()
+    }
+
+    companion object {
+        const val BUNDLE_KEY_FOR_DETAIL = "BUNDLE_KEY_FOR_DETAIL"
+    }
+}
