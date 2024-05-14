45ef6dd4ef75f03eb9c464c5216c223d8efcb1e1
HOCRUX\penci <pencil2486@gmail.com>
2024-05-14 09:31:54 +0900

    feat: HomeFragment RecyclerView 연결

--- app/src/main/java/com/nbc/youtube/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/nbc/youtube/presentation/home/HomeFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.nbc.youtube.data.repository.testRepo
 import com.nbc.youtube.databinding.FragmentHomeBinding
+import com.nbc.youtube.presentation.home.adapters.VideoAdapter
 
 
 class HomeFragment : Fragment() {
@@ -13,6 +16,8 @@ class HomeFragment : Fragment() {
     private var _binding: FragmentHomeBinding? = null
     private val binding: FragmentHomeBinding
         get() = _binding!!
+    private val popularVideoAdapter by lazy { VideoAdapter() }
+    private val categoryVideoAdapter by lazy { VideoAdapter() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,6 +25,7 @@ class HomeFragment : Fragment() {
     ): View {
         // Inflate the layout for this fragment
         _binding = FragmentHomeBinding.inflate(layoutInflater)
+        defaultRecyclerViewBind()
         return binding.root
     }
 
@@ -28,4 +34,25 @@ class HomeFragment : Fragment() {
 
         super.onDestroyView()
     }
+
+    private fun defaultRecyclerViewBind() {
+        rvMostPopularBind()
+        categoryBind()
+    }
+
+    private fun rvMostPopularBind() {
+        with(binding.rvMostPopular) {
+            adapter = popularVideoAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+        popularVideoAdapter.listUpdate(testRepo().getPopularVideos(),1)
+    }
+    private fun categoryBind() {
+        with(binding.rvCategoryVideo) {
+            adapter = categoryVideoAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+        }
+        categoryVideoAdapter.listUpdate(testRepo().getPopularVideos(),2)
+    }
+
 }
